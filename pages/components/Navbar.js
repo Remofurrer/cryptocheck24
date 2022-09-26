660d364c1b1545789289d05b2e39e681abc99410
remo furrer <furrer.remo@sunrise.ch>
2022-09-26 20:23:57 +0200

    new version

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,8 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
 
-import { CakeIcon, PlusCircleIcon } from '@heroicons/react/24/solid'
-
 const Navbar = () => {
   return (
     <div className='flex justify-between p-2'>
@@ -13,13 +11,6 @@ const Navbar = () => {
             <h2 className='font-semibold text-2xl text-blue-700 cursor-pointer'>Cryptocheck24</h2>
         </div>
         </Link>
-
-        {/* right side navbar */}
-        {/*         <div className='flex space-x-4 items-center'>
-            <PlusCircleIcon className='w-8 h-8 hover:text-blue-700 cursor-pointer'/>
-            <CakeIcon className='w-8 h-8 hover:text-blue-700 cursor-pointer'/>
-        </div> */}
-
     </div>
   )
 }
